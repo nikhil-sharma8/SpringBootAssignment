affab0d2da4c65c0c78f2f49b9ef6b28b088cfee
Nikhil Sharma <nikhil.sharma@zemosolabs.com>
2024-09-10 11:30:13 +0530

    :memo: chore: moved user application to root package

--- User/src/main/java/com/zemoso/user/UserApplication.java
+++ User/src/main/java/com/zemoso/user/UserApplication.java
@@ -1,4 +1,4 @@
-package com.zemoso.user.service;
+package com.zemoso.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
